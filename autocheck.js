ec0cf54df102ce5651e645b8931fb36f1040168c
Maxim Levandovskiy <90237902+oscuroplan@users.noreply.github.com>
2021-10-03 18:28:59 +0300

    Update autocheck.js

--- autocheck.js
+++ autocheck.js
@@ -1,4 +1,4 @@
-// задача 16/32
+                                                                                       // задача 16/32
 
 function makeArray(firstArray, secondArray, maxLength) {
     // Change code below this line
@@ -8,8 +8,30 @@ const message = newArray.length > maxLength ? newArray.slice(0, maxLength) : new
 
   return message;
 
-
     // Change code above this line
   }
 
-// задача 17/32
+
+
+                                                                                    // задача 17/32
+
+const start = 3;
+const end = 7;
+
+for (let i = start; i <= end; i += 1) { // Change this line
+  console.log(i);
+}
+
+                                                                                    // задача 18/32
+
+function calculateTotal(number) {
+ // Change code below this line
+  let total = 0;
+
+for (let i = 0; i <= number; i += 1) { // Change this line
+  total += i;
+}
+
+return total
+  // Change code above this line
+}
