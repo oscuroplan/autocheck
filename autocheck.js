25eefa8fc2d00f6fd0f108f2fe09921da83b987f
Maxim Levandovskiy <90237902+oscuroplan@users.noreply.github.com>
2021-10-03 19:41:07 +0300

    Update autocheck.js

--- autocheck.js
+++ autocheck.js
@@ -35,3 +35,39 @@ for (let i = 0; i <= number; i += 1) { // Change this line
 return total
   // Change code above this line
 }
+
+                                                                                    // задача 19/32
+
+const fruits = ['apple', 'plum', 'pear', 'orange'];
+
+for (let i = 0; i < fruits.length; i +=1) { // Change this line
+  const fruit = fruits[i]; // Change this line
+  console.log(fruit);
+}
+
+                                                                                    // задача 20/32
+function calculateTotalPrice(order) {
+  let total = 0;
+  // Change code below this line
+for (let i = 0; i < order.length; i += 1) {
+  total += order[i];
+}
+  // Change code above this line
+  return total;
+}
+
+                                                                                    // задача 21/32
+function findLongestWord(string) {
+  // Change code below this line
+let stringSplit= string.split(" ");
+ let longestWord= " "; 
+ for (let word of stringSplit){
+   if (word.length > longestWord.length) {
+   longestWord = word;
+   }
+ }
+return longestWord;
+  // Change code above this line
+}
+
+                                                                                    // задача 22/32
